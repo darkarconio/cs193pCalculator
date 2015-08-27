f105ee53c540a40cbf8f966798eaa5f9196ef65a
Michal Plucinski <michalplucinski@Michal-PC.local>
2015-08-26 18:00:20 -0600

    Stanford Swift Course winter 2015 assignment 1 complete

--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -8,18 +8,145 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var history: UILabel!
+    @IBOutlet weak var changeSignOutlet: UIButton!
+    
+    var userIsInTheMiddleOfTypingANumber = false
+    var operationStackIsClear = true
+    var equalSignPresentInHistory = false
+    
+    var brain = CalculatorBrain()
+    
+    @IBAction func appendDigit(sender: UIButton)
+    {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            if (display.text?.rangeOfString(".") == nil) || (digit != ".") {
+                display.text = display.text! + digit
+            }
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+    @IBAction func backspace(sender: UIButton)
+    {
+        if userIsInTheMiddleOfTypingANumber
+        {
+            display.text = dropLast(display.text!)
+            if count(display.text!) == 0
+            {
+                displayValue = nil
+            }
+        }
+    }
+    
+    @IBAction func changeSign(sender: UIButton)
+    {
+        if userIsInTheMiddleOfTypingANumber {
+            if let number = displayValue {
+                if number > 0 {
+                    display.text = "-" + display.text!
+                } else {
+                    display.text = dropFirst(display.text!)
+                }
+            }
+        } else {
+            operate(changeSignOutlet)
+        }
+    }
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func operate(sender: UIButton)
+    {
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        if let operation = sender.currentTitle
+        {
+            addToHistory("\(operation)", operation: true)
+            
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = nil
+            }
+        }
+    }
+    
+    @IBAction func enter()
+    {
+        userIsInTheMiddleOfTypingANumber = false
+        if let number = displayValue {
+            if let result = brain.pushOperand(number) {
+                displayValue = result
+                addToHistory("\(result)", operation: false)
+            } else {
+                displayValue = nil
+            }
+        } else {
+            displayValue = nil
+        }
+    }
+    
+    func addToHistory (addition: String, operation: Bool)
+    {
+        var equalSign = ""
+        
+        if equalSignPresentInHistory {
+            history.text = dropLast(history.text!)
+            history.text = dropLast(history.text!)
+            equalSignPresentInHistory = false
+        }
+        
+        if operation {
+            equalSign = " ="
+            equalSignPresentInHistory = true
+        }
+        
+        if operationStackIsClear {
+            history.text = addition + equalSign
+            operationStackIsClear = false
+        } else {
+            history.text = history.text! + " \(addition)" + equalSign
+        }
+    }
+    
+    
+    @IBAction func clear()
+    {
+        operationStackIsClear = true
+        
+        history.text = "\(0)"
+        displayValue = nil
+        
+        brain.clearStack()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    var displayValue: Double?
+    {
+        get {
+            var possibleNumber = NSNumberFormatter().numberFromString(display.text!)
+            
+            if let number = possibleNumber {
+                return number.doubleValue
+            } else {
+                return nil
+            }
+        }
+        set {
+            if let number = newValue {
+                display!.text = "\(number)"
+            }
+            else {
+                display!.text = "\(0)"
+            }
+            userIsInTheMiddleOfTypingANumber = false
+        }
     }
-
-
 }
 
